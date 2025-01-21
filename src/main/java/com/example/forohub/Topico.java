6b1f3f4b459107bb22c8692d6b573ef11b2b343a
Leo <leocasti2@gmail.com>
2025-01-21 03:38:45 -0500

    primer commit

---
+++ src/main/java/com/example/forohub/Topico.java
@@ -0,0 +1,87 @@
+package com.example.forohub;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "topico")
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String fechaCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+    public Topico() {
+    }
+
+    public Topico(Long id, String titulo, String mensaje, String fechaCreacion, String status, String autor,
+            String curso) {
+        this.id = id;
+        this.titulo = titulo;
+        this.mensaje = mensaje;
+        this.fechaCreacion = fechaCreacion;
+        this.status = status;
+        this.autor = autor;
+        this.curso = curso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
